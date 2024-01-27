064f6538301b79991c9f8ac0ce9d5bd579851aeb
Syed Fasih Uddin <sssfasih@gmail.com>
2024-01-27 21:46:34 +0500

    Time: 47 ms (7.26%), Space: 16.5 MB (75.22%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    #Fibonacci Sequence
+    cache = {}
+    def climbStairs(self, n: int) -> int:
+        if n in self.cache:
+            return self.cache[n]
+        
+        if n==1:
+            val = 1
+        elif n ==2:
+            val = 2
+        elif n > 2:
+            val = self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        self.cache[n] = val
+        return val
