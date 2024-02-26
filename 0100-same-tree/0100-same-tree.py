dbec49ad75b4351aeb506e0a9daaf80181a1cd45
Syed Fasih Uddin <sssfasih@gmail.com>
2024-02-26 17:44:29 +0500

    Time: 33 ms (75.87%), Space: 16.7 MB (45.81%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -12,18 +12,18 @@ class Solution:
             if node1 and node2 and node1.val == node2.val:
                 # we think that left and right are equal
                 a,b = True,True
-                #if left exists on both sides, traverse it to find equality
+                # if left exists on both sides, traverse it to find equality
                 if node1.left and node2.left:
                     a = traverse(node1.left,node2.left)
-                #if left exusts on one side only; return False
+                # if left exusts on one side only; return False
                 elif node1.left or node2.left:
                     return False
-                #same conditions as above for right
+                # same conditions as above for right
                 if node1.right and node2.right:
                     b= traverse(node1.right,node2.right)
                 elif node1.right or node2.right:
                     return False
-                #if our initial thinking was wrong, return False
+                # if our initial thinking was wrong, return False
                 if a == False or b == False:
                     return False
                 # if not returned False till now, return True
