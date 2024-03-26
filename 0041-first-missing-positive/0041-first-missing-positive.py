6117f446ad4c7a0260d8d9655a44d2ecc8eff60f
Syed Fasih Uddin <sssfasih@gmail.com>
2024-03-26 17:32:10 +0500

    Time: 319 ms (14.25%), Space: 30.2 MB (81.41%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -8,9 +8,8 @@ class Solution:
             i+=1
 
         for i in range(nums_len):
-            if abs(nums[i]) != nums_len+1 and nums[i] <= nums_len:
-                if nums[abs(nums[i])-1] > 0:
-                    nums[abs(nums[i])-1] *= -1
+            if abs(nums[i]) <= nums_len:
+                nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1])
                 
         for i in range(nums_len):
             if nums[i] > 0:
