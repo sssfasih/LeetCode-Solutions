cd9cc414a71e22e09642fb9f966438a2324cfc9e
Syed Fasih Uddin <sssfasih@gmail.com>
2024-04-05 15:58:49 +0500

    Time: 34 ms (78.84%), Space: 16.6 MB (11.30%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,18 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        i=0
+        while i<len(s)-1:
+            if s[i] in 'abcdefghijklmnopqrstuvwxyz' and s[i+1] == s[i].capitalize():
+                s= s[:i:] +s[i+2::]
+                if i>0:
+                    i-=1 
+                continue
+            
+            if s[i] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' and s[i+1] == s[i].lower():
+                s= s[:i:] +s[i+2::]
+                if i>0:
+                    i-=1
+                continue
+            i+=1
+        return s
+        
