20be7782a3b1b8447fac13bef2662323ebd3784a
Syed Fasih Uddin <sssfasih@gmail.com>
2024-04-05 16:29:38 +0500

    Time: 37 ms (61.53%), Space: 16.5 MB (91.04%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -2,7 +2,7 @@ class Solution:
     def makeGood(self, s: str) -> str:
         i=0
         while i<len(s)-1:
-            if (s[i].islower() and s[i+1] == s[i].capitalize()) or (s[i].isupper() and s[i+1] == s[i].lower()):
+            if (s[i] != s[i+1] and s[i].lower() == s[i+1].lower()):
                 s= s[:i:] +s[i+2::]
                 if i>0:
                     i-=1 
