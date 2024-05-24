f6bde7cfe4290bd64d4466697cf1d5a47cb6e7b4
Syed Fasih Uddin <sssfasih@gmail.com>
2024-05-24 21:35:44 +0500

    Time: 30 ms (95.32%), Space: 16.4 MB (99.64%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def valid(self, s, start, length):
+        return length == 1 or (
+            s[start] != "0"
+            and (length < 3 or s[start : start + length] <= "255")
+        )
+
+    # main helper method to solve the problem by backtracking
+    def helper(self, s, startIndex, dots, ans):
+        remainingLength = len(s) - startIndex
+        remainingNumberOfIntegers = 4 - len(dots)
+        if (
+            remainingLength > remainingNumberOfIntegers * 3
+            or remainingLength < remainingNumberOfIntegers
+        ):
+            return
+        
+        if len(dots) == 3:
+            if self.valid(s, startIndex, remainingLength):
+                temp = ""
+                last = 0
+                for dot in dots:
+                    temp += s[last : last + dot] + "."
+                    last += dot
+                temp += s[startIndex:]
+                ans.append(temp)
+            return
+        for curPos in range(1, min(4, remainingLength + 1)):
+            dots.append(curPos)
+            if self.valid(s, startIndex, curPos):
+                self.helper(s, startIndex + curPos, dots, ans)
+            dots.pop()
+
+    # main method called by leetcode
+    def restoreIpAddresses(self, s):
+        answer = []
+        self.helper(s, 0, [], answer)
+        return answer
